e362783c60a1ca22189f6e3b1ac2ebdc4515c549
tianxiaoye <txy_991226@126.com>
2022-02-23 01:28:38 +0800

    button添加

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/index.css';
 import router from './router/index'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 const app = createApp(App);
+
+app.use(ElementPlus)
 app.use(router)
 app.mount('#app')
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,21 @@
-import { createRouter, createWebHashHistory} from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 
 const router = createRouter({
     history: createWebHashHistory(),
     routes: [
         {
-            path:'/',
-            redirect:'/home'
+            path: '/',
+            redirect: '/home'
         },
         {
-            path:'/home',
-            name:'home',
+            path: '/home',
+            name: 'home',
             component: () => import('@/views/home.vue')
+        },
+        {
+            path: '/button',
+            name: 'button',
+            component: () => import('@/views/button.vue')
         }
     ]
 })
